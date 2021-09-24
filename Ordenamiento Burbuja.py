b1fce6103e246b625de3cf3d36b63894d6ebb6e2
JEFERSONCHHU05 <90531248+JEFERSONCHHU05@users.noreply.github.com>
2021-09-24 09:14:36 -0500

    Update Ordenamiento Burbuja.py

--- Ordenamiento Burbuja.py
+++ Ordenamiento Burbuja.py
@@ -11,17 +11,23 @@ b=[4,1,7,3,7,0,1,3,8]
 def metodo_burbuja(vector):
     permutation = True
     iteracion = 0
+    #Hace la comparacion desde el primero al ultimo si hay un intercambio
+    #Esto continuara hasta no haber intercambio
     while permutation == True:
         permutation = False
         iteracion = iteracion + 1
         # Explora la lista
         for actual in range(0, len(vector) - iteracion):
+            #Intercambiamos los dos elementos y los actualiza
             if vector[actual] > vector[actual + 1]:
                 permutation = True
-                # Intercambiamos los dos elementos
                 vector[actual], vector[actual + 1] = \
                 vector[actual + 1],vector[actual]
+                #Mostrar la comparacion de izquierda a derecha
+                print(vector[actual + 1],"se compara a", vector[actual])
     return vector
 #Mostrar el vector en forma ordenada
+print(a)
 print(metodo_burbuja(a))
+print(b)
 print(metodo_burbuja(b))
